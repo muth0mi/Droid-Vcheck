63a0aafe9616c0fda4e1e2092a9915e2ae31fae0
Oliver Muthomi <oliver.mwitia@gmail.com>
2019-09-16 12:57:06 +0300

    Implement normal application flow

---
+++ app/src/main/java/io/muth0mi/droid_vcheck/Application.kt
@@ -0,0 +1,12 @@
+package io.muth0mi.droid_vcheck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Application : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_application2)
+    }
+}
--- app/src/main/java/io/muth0mi/droid_vcheck/MainActivity.kt
+++ app/src/main/java/io/muth0mi/droid_vcheck/MainActivity.kt
@@ -1,12 +1,30 @@
 package io.muth0mi.droid_vcheck
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize views
+        val spnVersion = findViewById<Button>(R.id.btn_emulate)
+        val btnProceed = findViewById<Button>(R.id.btn_emulate)
+
+
+        // Check version in coroutine and proceed to application
+        btnProceed.setOnClickListener {
+            checkIfVersionIsUptodate()
+            // Proceed to application
+            startActivity(Intent(this, Application::class.java))
+        }
+    }
+
+    private fun checkIfVersionIsUptodate() {
+//        GlobalSc
     }
 }
