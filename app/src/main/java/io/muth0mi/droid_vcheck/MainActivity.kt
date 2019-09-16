db345456e1702e1b812b90ec51171291cda400ee
Oliver Muthomi <oliver.mwitia@gmail.com>
2019-09-16 11:42:03 +0300

    Create project in Android Studio

---
+++ app/src/main/java/io/muth0mi/droid_vcheck/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.muth0mi.droid_vcheck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
